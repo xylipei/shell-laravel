974345e56b28f3ec75a8601c5dc97a146a4e2c23
Your Name <you@example.com>
2019-12-11 16:08:12 +0800

    话题列表页面

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -1,9 +1,32 @@
 <?php
-
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
 use Faker\Generator as Faker;
 
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+
+    $sentence = $faker->sentence();
+
+    // 随机取一个月以内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+
+    // 传参为生成最大时间不超过，因为创建时间需永远比更改时间要早
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
