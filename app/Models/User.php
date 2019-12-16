ca1ca6b5bd229129e0e9201ebbe6ea0a14cf6516
Your Name <you@example.com>
2019-12-16 18:00:38 +0800

    用户最后活跃时间

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,6 +38,8 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
+    use Traits\LastActivedAtHelper;
+
     use Traits\ActiveUserHelper;
 
     use HasRoles;
